a39d515863ac8bb8e000a518312076bfa5a65929
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-03-19 20:04:12 +0200

    add passwork confirmation

--- src/components/auth/signUpForm.vue
+++ src/components/auth/signUpForm.vue
@@ -1,16 +1,17 @@
 <template>
-    <v-form>
-        <v-row>
-            <v-text-field type="email" label="Email" name="email" id="email">
-            </v-text-field>
-        </v-row>
-        <v-row>
-            <v-text-field type="password" label="password" name="password" id="password">
-            </v-text-field>
-        </v-row>
-        <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn type="submit" color="primary">sign up</v-btn>
-        </v-card-actions>
-    </v-form>
+<v-form>
+    <v-row>
+        <v-text-field type="email" label="Email" name="email" id="email" />
+    </v-row>
+    <v-row>
+        <v-text-field type="password" label="Password" name="password" id="password" />
+    </v-row>
+    <v-row>
+        <v-text-field type="password" label="Confirm Password" name="onfirmPassword" id="confirmPassword" />
+    </v-row>
+    <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn type="submit" color="primary">sign up</v-btn>
+    </v-card-actions>
+</v-form>
 </template>

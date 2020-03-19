b532906da339eddd6f020f92b88e4ada0695181d
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-03-19 19:57:59 +0200

    adding sign up UI

---
+++ src/components/auth/signUpForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-form>
+        <v-row>
+            <v-text-field type="email" label="Email" name="email" id="email">
+            </v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field type="password" label="password" name="password" id="password">
+            </v-text-field>
+        </v-row>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" color="primary">sign up</v-btn>
+        </v-card-actions>
+    </v-form>
+</template>
--- src/views/sigunUp.vue
+++ src/views/sigunUp.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        sigun up view
-    </div>
+    <v-container>
+        <v-row justify="center" align="center">
+            <v-col xs="12" sm="6"  offset-sm3>
+                <v-card>
+                    <v-card-title>
+                        Sign Up for Sheet Views
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <signUpForm />
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+<script>
+import signUpForm from '@/components/auth/signUpForm'
+export default {
+    components:{
+        signUpForm
+    }
+}
+</script>

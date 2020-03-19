b9aa19513f0473f17645e1f6b91edd7843283eba
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-03-19 20:29:54 +0200

    form validation

--- src/components/auth/signUpForm.vue
+++ src/components/auth/signUpForm.vue
@@ -1,13 +1,13 @@
 <template>
-<v-form>
+<v-form ref="form" v-model="valid" lazy-validation>
     <v-row>
-        <v-text-field type="email" label="Email" name="email" id="email" />
+        <v-text-field type="email" :rules="emailRules" v-model="user.mail" label="Email" name="email" id="email" />
     </v-row>
     <v-row>
-        <v-text-field type="password" label="Password" name="password" id="password" />
+        <v-text-field type="password" :rules="passwordRules" v-model="user.password" label="Password" name="password" id="password" />
     </v-row>
     <v-row>
-        <v-text-field type="password" label="Confirm Password" name="onfirmPassword" id="confirmPassword" />
+        <v-text-field type="password" :rules="passwordConfimationRules" label="Confirm Password" name="onfirmPassword" id="confirmPassword" />
     </v-row>
     <v-card-actions>
         <v-spacer></v-spacer>
@@ -15,3 +15,27 @@
     </v-card-actions>
 </v-form>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                name: '',
+                password: ''
+            },
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v => (v && v.length >= 6) || 'Password must be greater than 6 characters',
+            ],
+            passwordConfimationRules: [
+                v => (v && v == this.user.password) || 'Password not match',
+            ],
+        }
+    },
+}
+</script>

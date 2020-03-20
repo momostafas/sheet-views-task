ab8acb22fa6f40db63c64617f661300bd1c9f00b
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-03-20 19:18:18 +0200

    displaying data

---
+++ src/components/home/dataTable.vue
@@ -0,0 +1,22 @@
+<template>
+   <v-data-table
+      :headers="headers"
+      :items="data"
+    ></v-data-table>
+</template>
+<script>
+import data from '@/imports/data.json'
+export default {
+    data() {
+        return {
+            headers:[
+                {text: 'name', value: 'name'},
+                {text: 'age', value: 'age'},
+                {text: 'address', value: 'address'},
+                {text: 'phone', value: 'phone'},
+            ],
+            data: data
+        }
+    },
+}
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+    <v-card>
+        <v-card-title>
+            Sheet Views Data
+        </v-card-title>
+        <v-card-text>
+            <dataTable />
+        </v-card-text>
+    </v-card>
+</v-container>
+</template>
+<script>
+import dataTable from '@/components/home/dataTable'
+export default {
+    components: {
+        dataTable
+    }
+}
+</script>
